5caf21a8ccf05922eb7fa43ab654735b0bdbb034
shruti kumari <kshruti177@gmail.com>
2024-10-23 10:03:11 +0000

    schema updated expanded issue solved

--- db/schema.cds
+++ db/schema.cds
@@ -29,27 +29,27 @@ entity COLLECTIONLOC  {
 
 
 entity RIMS_REQUEST {
-    key RequestCode : Integer; // Unique identifier for each request
-    typeOfRequest       : String(10); // Type of the request
-    collectionLocationType : String(10); // Type of collection location
-    well                : String(50); // Associated well
-    wBS                 : String(20); // Work Breakdown Structure (WBS)
-    requesterLocation   : String(50); // Location of the requester
-    projectType         : String(10); // Type of project
-    remarks             : String(255); // Any remarks associated with the request
+    key RequestCode     : Integer; 
+    typeOfRequest       : String(10); 
+    collectionLocationType : String(10); 
+    well                : String(50); 
+    wBS                 : String(20); 
+    requesterLocation   : String(50);
+    projectType         : String(10); 
+    remarks             : String(255);
     
     
-    materials           : Composition of many Material on materials.RequestCode = $self;
+    materials           : Composition of many Material on materials.RequestCode = $self
 }
 
 
 entity Material {
-    key itemCode   : String(10); // Unique identifier for each material
-    description    : String(255); // Description of the material
-    uOM            : String(5); // Unit of Measure
-    quantity       : Decimal(15,3); // Quantity of the material
+    key itemCode   : String(10); 
+    description    : String(255); 
+    uOM            : String(5); 
+    quantity       : Decimal(15,3); 
     remarks_1      : String(255); 
-    RequestCode : Association to one RIMS_REQUEST
+    key RequestCode : Association to RIMS_REQUEST;
     
    
 }
--- srv/service.js
+++ srv/service.js
@@ -2,40 +2,47 @@ const cds = require('@sap/cds');
  
  
 module.exports = async ( srv) => {
-    srv.on('READ', 'WELL', req => {
-        return cds.run(SELECT.from('mydb.WELL'));
-    });
+    // srv.on('READ', 'WELL', req => {
+    //     return cds.run(SELECT.from('mydb.WELL'));
+    // });
    
-    srv.on('READ', 'WBS', req => {
-        return cds.run(SELECT.from('mydb.WBS'));
+    // srv.on('READ', 'WBS', req => {
+    //     return cds.run(SELECT.from('mydb.WBS'));
+    // });
+
+    // srv.on('READ', 'COLLECTIONLOC', req => {
+    //     return cds.run(SELECT.from('mydb.COLLECTIONLOC'));
+    // });
+
+    // srv.on('READ', 'REQUESTTYPE', req => {
+    //     return cds.run(SELECT.from('mydb.REQUESTTYPE'));
+    // });
+    // srv.on('READ', 'PROJECTTYPE', req => {
+    //     return cds.run(SELECT.from('mydb.PROJECTTYPE'));
+    // });
+    // // srv.on('READ', 'Material', req => {
+    //     return cds.run(SELECT.from('mydb.Material'));
+    // });
+    srv.on('CREATE', 'RIMS_REQUEST',async req => {
+        await cds.run(INSERT.into('mydb.RIMS_REQUEST').entries(req.data));
+        return
     });
-    srv.on('READ', 'COLLECTIONLOC', req => {
-        return cds.run(SELECT.from('mydb.COLLECTIONLOC'));
-    });
-    srv.on('READ', 'REQUESTTYPE', req => {
-        return cds.run(SELECT.from('mydb.REQUESTTYPE'));
-    });
-    srv.on('READ', 'PROJECTTYPE', req => {
-        return cds.run(SELECT.from('mydb.PROJECTTYPE'));
-    });
-    srv.on('READ', 'RIMS_REQUEST', req => {
-        return cds.run(SELECT.from('mydb.RIMS_REQUEST'));
-    });
-    srv.on('READ', 'Material', req => {
-        return cds.run(SELECT.from('mydb.Material'));
-    });
-    srv.on('CREATE', 'RIMS_REQUEST', req => {
-        return cds.run(INSERT.into('mydb.RIMS_REQUEST').entries(req.data));
+    srv.on('CREATE', 'WELL', async req => {
+        await cds.run(INSERT.into('mydb.WELL').entries(req.data));
+        return cds.run(SELECT.from('mydb.WELL').where({WellCode : req.data.WellCode}))
     });
     srv.on('CREATE', 'Material', req => {
         return cds.run(INSERT.into('mydb.Material').entries(req.data));
     });
- 
+    
     srv.on('UPDATE', 'Material', req => {
         return cds.run(UPDATE('mydb.Material').set(req.data).where({ ID: req.data.ID }));
     });
-    srv.on('UPDATE', 'RIMS_REQUEST', req => {
-        return cds.run(UPDATE('mydb.RIMS_REQUEST').set(req.data).where({ ID: req.data.ID }));
-    });
-
-}
+    // srv.on('UPDATE', 'RIMS_REQUEST', req => {
+        //     return cds.run(UPDATE('mydb.RIMS_REQUEST').set(req.data).where({ ID: req.data.ID }));
+        // });
+        
+    }
+    // srv.on('READ', 'RIMS_REQUEST', req => {
+    //     return cds.run(SELECT.from('mydb.RIMS_REQUEST'));
+    // });
